7b16e44c0eea87b4017dbbb4ba655d4117adf98d
Santinilestari Energi Indonesia <it.santinilestari@gmail.com>
2021-02-10 16:48:11 +0700

    initial commit

---
+++ app/src/main/java/com/example/app_bar_top/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.app_bar_top
+
+//https://material.io/components/app-bars-top/android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
